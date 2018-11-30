df4a1a8f7677328a3ba2f7fcf984f05762bf3c12
Aoo Studio <39947616+aoostudio@users.noreply.github.com>
2018-11-30 22:46:55 +0700

    Update update-cent7.sh

--- update-cent7.sh
+++ update-cent7.sh
@@ -1,8 +1,10 @@
-yum -y update
-
 # install
 yum install screen -y
+screen
 yum install perl -y
 yum install net-tools -y
 yum install apt -y
 yum install open-vm-tools -y
+
+# update
+yum -y update
