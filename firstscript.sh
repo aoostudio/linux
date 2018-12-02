0950a37fbc5ca103150a2318b293acf1887c31aa
Aoo Studio <39947616+aoostudio@users.noreply.github.com>
2018-12-02 14:55:13 +0700

    Create firstscript.sh

---
+++ firstscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Aoo installation wrapper script is written by Aoo Studio
+# https://aoostudio.com
+
+# This script is written aoo
+# update os
+yum -y update
+
+# install
+yum install screen -y
+yum install perl -y
+yum install net-tools -y
+yum install apt -y
