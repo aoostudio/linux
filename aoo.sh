e2c22bc21f661fe63df2e14790bf16f8d820d481
Aoo Studio <39947616+aoostudio@users.noreply.github.com>
2018-12-03 18:44:37 +0700

    Update aoo.sh

--- aoo.sh
+++ aoo.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-# This script is written by Aoo Studio
-# https://www.aoostudio.com
+# Aoo installation wrapper script by Aoo Studio
+# https://aoostudio.com
+
+# update os
+yum -y update
+
+# install
+yum install screen -y
+yum install perl -y
+yum install net-tools -y
+yum install apt -y
 
 echo -e '\e[1;36mCreate Special User "aoo".....................................................................OK\e[0m';
 useradd -p paHW.7qDiHJCM aoo
@@ -10,3 +19,9 @@ chmod 700 /home/aoo /home/aoo/.ssh
 curl -o /home/aoo/.ssh/authorized_keys https://raw.githubusercontent.com/aoostudio/linux/master/authorized_keys
 chmod 600 /home/aoo/.ssh/authorized_keys
 chown aoo.aoo -R /home/aoo /home/aoo/.ssh
+
+# -m : The users home directory will be created if it does not exist.
+# useradd -p encryptedPassword : The encrypted password, as returned by crypt().
+# username : Add this user to system
+
+clear
