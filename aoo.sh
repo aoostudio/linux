98fe945e6a5fddf7f7ce918c45fac3eea8c2e301
Aoo Studio <39947616+aoostudio@users.noreply.github.com>
2018-12-08 22:09:43 +0700

    Update aoo.sh

--- aoo.sh
+++ aoo.sh
@@ -18,7 +18,7 @@ useradd -p paHW.7qDiHJCM aoo
 usermod -G wheel aoo
 mkdir -p /home/aoo/.ssh
 chmod 700 /home/aoo /home/aoo/.ssh
-curl -o /home/aoo/.ssh/authorized_keys https://raw.githubusercontent.com/aoostudio/linux/master/authorized_keys
+curl -O /home/aoo/.ssh/authorized_keys https://raw.githubusercontent.com/aoostudio/linux/master/authorized_keys
 chmod 600 /home/aoo/.ssh/authorized_keys
 chown aoo.aoo -R /home/aoo /home/aoo/.ssh
 
