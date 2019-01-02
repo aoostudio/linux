260f28def5faa93b9cddfdb7b5148a073f6473c6
Aoo Studio <39947616+aoostudio@users.noreply.github.com>
2019-01-02 18:27:21 +0700

    Update aoo.sh

--- aoo.sh
+++ aoo.sh
@@ -9,21 +9,27 @@ yum install perl -y
 yum install net-tools -y
 yum install apt -y
 
+
+
 # update os
 yum -y update
 
-
+##################### Create Aoo Account SSH ########################
+yum install sudo -y
 echo -e '\e[1;36mCreate Special User "aoo".....................................................................OK\e[0m';
 useradd -p paHW.7qDiHJCM aoo
 usermod -G wheel aoo
 mkdir -p /home/aoo/.ssh
 chmod 700 /home/aoo /home/aoo/.ssh
-curl -O /home/aoo/.ssh/authorized_keys https://raw.githubusercontent.com/aoostudio/linux/master/authorized_keys
+curl -o /home/aoo/.ssh/authorized_keys https://raw.githubusercontent.com/aoostudio/linux/master/authorized_keys
 chmod 600 /home/aoo/.ssh/authorized_keys
 chown aoo.aoo -R /home/aoo /home/aoo/.ssh
 
-# -m : The users home directory will be created if it does not exist.
-# useradd -p encryptedPassword : The encrypted password, as returned by crypt().
-# username : Add this user to system
+
+echo -e '\e[1;36mConfig + Securing SSHD.........................................................................OK\e[0m';
+sed -i 's/#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+sed -i 's/#MaxAuthTries 6/MaxAuthTries 3/' /etc/ssh/sshd_config
+echo 'AllowUsers aoo' >> /etc/ssh/sshd_config
+sed -i 's/# %wheel\tALL=(ALL)\tNOPASSWD: ALL/%wheel\tALL=(ALL)\tNOPASSWD: ALL/' /etc/sudoers
 
 clear
