4594a36f2a0dc1ded866f9910280fb2d24be6a5f
Aoo Studio <39947616+aoostudio@users.noreply.github.com>
2019-02-16 13:13:35 +0700

    Update validate_ip.sh

--- validate_ip.sh
+++ validate_ip.sh
@@ -5,17 +5,21 @@
 # Meant for user input validation.
 
 function validate_IP() {
+        # Step 1 : Contain exactly three dots.
 if [ `echo $1 | grep -o '\.' | wc -l` -ne 3 ]; then
         echo "Parameter '$1' does not look like an IP Address (does not contain 3 dots).";
         exit 1;
+        # Step 2 : Contain exactly four parts, if broken down by dots.
 elif [ `echo $1 | tr '.' ' ' | wc -w` -ne 4 ]; then
         echo "Parameter '$1' does not look like an IP Address (does not contain 4 octets).";
         exit 1;
-else
+else       
+        # 3 All four parts are numeric.
         for OCTET in `echo $1 | tr '.' ' '`; do
                 if ! [[ $OCTET =~ ^[0-9]+$ ]]; then
                         echo "Parameter '$1' does not look like in IP Address (octet '$OCTET' is not numeric).";
-                        exit 1;
+                        exit 1;   
+        # Step 4 : All four parts are between 0 and 255.
                 elif [[ $OCTET -lt 0 || $OCTET -gt 255 ]]; then
                         echo "Parameter '$1' does not look like in IP Address (octet '$OCTET' is not in range 0-255).";
                         exit 1;
