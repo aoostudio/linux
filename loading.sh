54f4bb0cf5c4e23f0d0dfdf96ee5f514cfdd30dd
Aoo Studio <39947616+aoostudio@users.noreply.github.com>
2019-02-24 00:04:13 +0700

    Create loading.sh
    #!/bin/bash
    # Aoo Loading wrapper # Version 1.0.0.1
    # Script Developed by Apivat Pattana-Anurak
    # SysAdmin & Programmer # Thailand # Bangkok
    # https://www.aoostudio.com
    # Download : curl -O https://raw.githubusercontent.com/aoostudio/linux/master/loading.sh.sh
    # Install : sh loading.sh

---
+++ loading.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Aoo Loading wrapper # Version 1.0.0.1
+# Script Developed by Apivat Pattana-Anurak
+# SysAdmin & Programmer # Thailand # Bangkok
+# https://www.aoostudio.com
+# Download : curl -O https://raw.githubusercontent.com/aoostudio/linux/master/loading.sh.sh
+# Install : sh loading.sh
+
+echo -ne 'A                         (10%)\r'
+sleep 1
+echo -ne 'Aoo                       (20%)\r'
+sleep 1
+echo -ne 'Aoo P                     (30%)\r'
+sleep 1
+echo -ne 'Aoo Pat                   (40%)\r'
+sleep 1
+echo -ne 'Aoo Patta                 (50%)\r'
+sleep 1
+echo -ne 'Aoo Pattana               (60%)\r'
+sleep 1
+echo -ne 'Aoo Pattana-a             (70%)\r'
+sleep 1
+echo -ne 'Aoo Pattana-anu           (80%)\r'
+sleep 1
+echo -ne 'Aoo Pattana-anur          (90%)\r'
+sleep 1
+echo -ne 'Aoo Pattana-anurak       (100%)\r'
+echo -ne '\n'
